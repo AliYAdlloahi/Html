a7a8057042d870a5c259739270a33ab1dc4c1527
Ali Yadollahi <aliYadollahi683@gmail.com>
2023-01-12 23:57:52 +0330

    class and css selctors

---
+++ app.js
@@ -0,0 +1,7 @@
+let bahre = prompt("enter your bahre");
+let mogodi = prompt("enter your mogodi avalie");
+let month = prompt("enter your month");
+
+for (let i = 1; i <= 12; i++) {
+  console.log(alert());
+}
